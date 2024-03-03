8dab22cd5c5d6b201c402a4fe01679dcc667b29a
Will Berriss <10340373+willberriss@users.noreply.github.com>
2024-03-03 10:10:53 +0000

    Added initial googletest unittest.

---
+++ textadventureglitch_unittest/tst_testcase1.cpp
@@ -0,0 +1,14 @@
+
+#include <gtest/gtest.h>
+#include <gmock/gmock-matchers.h>
+
+#include "../../../repos_github_mine/TextAdventureGlitch/commandparser.h"
+
+using namespace testing;
+
+TEST(TestSuite1, TestCase1)
+{
+    EXPECT_EQ(1, 1);
+    ASSERT_THAT(0, Eq(0));
+}
+
